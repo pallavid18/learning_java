bb70356d349e698a2dedfe4dfceb91e8b919559b
pallavid18 <pallavidhope9@gmail.com>
2025-11-19 18:16:58 +0530

    variable test

---
+++ variableTest.java
@@ -0,0 +1,27 @@
+class variableTest
+{
+	public static void main(String[]args)
+	   {
+		byte a = 1;
+		byte A = 2;
+		short b = 10 ;
+		int num = 34;
+		char ch = 's';
+		float ht = 5.5555f;
+		double d = 3.142;
+		String name = "Siya";
+		long mobNo = 987456321;
+		boolean status = true;
+		
+		System.out.println("Value of byte   : " +a);
+		System.out.println("Value of byte   : " +A);
+		System.out.println("Value of short  : " +b);
+		System.out.println("Value of int    : " +num);
+		System.out.println("Value of char   : " +ch);
+		System.out.println("Value of float  : " +ht);
+		System.out.println("Value of double : " +d);
+		System.out.println("Value of String : " +name);
+		System.out.println("Value of long   : " +mobNo);
+		System.out.println("Value of boolean   : " +status);
+	   }
+}
