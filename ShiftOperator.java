a8bf670c9d25ab3d4a4e1db3b7ac0ecdfb0d4865
pallavid18 <pallavidhope9@gmail.com>
2025-11-21 18:21:28 +0530

    operator and commandline

---
+++ ShiftOperator.java
@@ -0,0 +1,13 @@
+class ShiftOperator
+{
+	public static void main(String[]args)
+	{
+		int a = 10;
+		System.out.println("Left Shift   : "+(a << 1));
+		System.out.println("Left Shift   : "+(a << 2));
+		System.out.println("Left Shift   : "+(a << 3));
+		System.out.println("Right Shift  : "+(a >> 1));
+		System.out.println("Right Shift  : "+(a >> 2));
+		System.out.println("Right Shift  : "+(a >> 3));
+	}
+}
---
+++ TestBitwiseOp.java
@@ -0,0 +1,15 @@
+class TestBitwiseOp
+{
+	public static void main(String[]args)
+	{
+		int a = 10;
+		int b = 9;
+
+		System.out.println("Bitwise AND(a&b)  : "+(a&b));
+		System.out.println("Bitwise OR (a|b)  : "+(a|b));
+		System.out.println("Bitwise XOR(a^b)  : "+(a^b));
+		System.out.println("Bitwise NOT(~b)   : "+(~b));
+		System.out.println("Bitwise AND(~a)   : "+(~a));
+		System.out.println("Bitwise AND(~a)   : "+(a<<1));
+	}
+}
