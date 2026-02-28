68a5f3475ec5ea9c7b4cfca32b24d75f9488a9f2
pallavid18 <pallavidhope9@gmail.com>
2026-02-28 15:29:52 +0530

    index done

---
+++ index.java
@@ -0,0 +1,7 @@
+public class index {
+    public static void main(String[] args) {
+        System.out.println("Pallavi");
+    }
+}
+
+//dev ---> java code  .java --> complier(javac) --> Byte code (.class)--> jdk-->JRE-->JVM (Java Virtual Machine) --> OS (Operating System)
